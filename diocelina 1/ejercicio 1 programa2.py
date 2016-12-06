2f7ccd898290481524386dc77786d90257114929
diocelinas561 <diocelinas561@gmail.com>
2016-12-06 08:56:42 -0600

    Add files via upload

---
+++ diocelina 1/ejercicio 1 programa2.py
@@ -0,0 +1,9 @@
+#Diocelina Sierra Martinez
+#27/11/16
+#Ejercicio 1 programa 2
+a=input("calcular la hipotenusa del triangulo: ")
+c=input("ingresar el cateto c: ")
+import math
+hipotenusa=a**2+c**2
+raiz=math.sqrt(hipotenusa)
+print "la hipotenusa del triangulo es: " , raiz
---
+++ diocelina 1/ejercicio 2 programa2.py
@@ -0,0 +1,8 @@
+#Diocelina Sierra Martinez
+#27/11/16
+#ejercicio 2 programa 2
+km=input("ingresar km que vas recorrer: ")
+m=km*1000
+tarifa=8.74
+distancia=(m/250)*1.84+8.74
+print "por la distancia que quiere recorrer debe pagar", distancia, "pesos"
